133c62d5d08cd364d6b43f1a0e2940cc2d38e605
Marcin <marcin.szturminski@gmail.com>
2024-02-01 11:51:04 +0100

    working adding Sticky Note on click

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,32 @@ import { useState } from 'react'
 import StickyNote from './components/StickyNote'
 
 function App() {
+	const [listOfStickyNotes, setListOfStickyNotes] = useState([])
+
+	function addStickyNoteFunction() {
+		const newStickyNote = {
+			id: Math.random(),
+			removeStickyNoteFunction: () => {},
+		}
+
+		const updatedListOfStickyNotes = [...listOfStickyNotes, newStickyNote]
+		let ids = updatedListOfStickyNotes.map(id => id.id)
+		console.log(ids)
+		console.log(updatedListOfStickyNotes)
+		setListOfStickyNotes(updatedListOfStickyNotes)
+	}
+
 	return (
 		<div className='app'>
 			<div className='notes__wrapper'>
 				<StickyNote></StickyNote>
+				{listOfStickyNotes.map(id => (
+					<StickyNote key={id.id}>{id.id}</StickyNote>
+				))}
 			</div>
-
-			<button className='btn__add'>Add New Note</button>
+			<button className='btn__add' onClick={addStickyNoteFunction}>
+				Add New Note
+			</button>
 		</div>
 	)
 }
